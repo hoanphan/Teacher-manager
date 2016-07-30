d84b282796e97f5fa46eb8f38e3533d34d009025
Hoan <dragonknight.k54dhcntt@gmail.com>
2016-07-30 19:37:19 +0700

    design backend

---
+++ protected/modules/admin/components/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: HOANDHTB
+ * Date: 7/26/2016
+ * Time: 4:47 PM
+ */
+class AdminController extends CController
+{
+    public $pageTitle;
+    public $layout='main';
+    public $breadcrumbs=array();
+    public $menu;
+}
---
+++ protected/modules/admin/components/BreadCrumbs.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: HOANDHTB
+ * Date: 7/30/2016
+ * Time: 4:24 PM
+ */
+class BreadCrumbs
+{
+
+}
